b17dca1a9972378c630b5e6e682948238f1376b4
Ridercogswell <ridercogswell@gmail.com>
2022-09-01 19:10:27 -0700

    spanish/frenc test

--- hello.go
+++ hello.go
@@ -2,12 +2,24 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const english = "English"
+const french = "French"
 const englishHelloPrefix = "hello, "
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		return spanishPrefix + name
+	} else if language == french {
+		return frenchPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
