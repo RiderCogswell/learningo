8f1edd5b33c6cf35c323ca54c057fd0cbaf633a8
Ridercogswell <ridercogswell@gmail.com>
2022-09-01 18:46:00 -0700

    refactor tests

--- hello_test.go
+++ hello_test.go
@@ -14,9 +14,12 @@ func TestHello(t *testing.T) {
 	t.Run("say 'hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "hello, World"
+		assertCorrectMessage(t, got, want)
+}
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
