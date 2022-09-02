92cab51595a45fdb59f9dd25ca380432c949cc88
Ridercogswell <ridercogswell@gmail.com>
2022-09-01 22:08:45 -0700

    test for coverage

--- sum/sum.go
+++ sum/sum.go
@@ -1,6 +1,6 @@
 package main 
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int { // arrays lengths must be declared on init, so instead we use a slice for flexibility
 	sum := 0
 	// range iterates over an array and returns the index, and the value
 	for _, number := range numbers { // we are choosing to ignore the iterator by using the blank identifier => _
