5fe9350b9509be2f70f9881d8c1b1cf942c063b9
Ridercogswell <ridercogswell@gmail.com>
2022-09-01 21:55:27 -0700

    refactor using range

--- sum/sum.go
+++ sum/sum.go
@@ -2,8 +2,9 @@ package main
 
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	// range iterates over an array and returns the index, and the value
+	for _, number := range numbers { // we are choosing to ignore the iterator by using the blank identifier => _
+		sum += number
 	}
 	return sum
 }
